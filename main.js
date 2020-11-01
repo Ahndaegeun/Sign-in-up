2d06cf2de87ea71b139c2ad031432bfad079643c
kade <adg0807@naver.com>
2020-11-01 23:54:49 +0900

    main design

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.getElementsByClassName('first').onclick = function () {
+        alert('h');
+    }; 
+    document.getElementById('second').onclick = function () {
+        alert('p');
+    };
+    document.getElementById('third').onclick = function () {
+        alert('i');
+    };
+};
